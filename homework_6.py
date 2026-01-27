14fd818d51e5b770c2ff190899fba38a61317c78
Maria <er.masha02@gmail.com>
2026-01-27 18:23:39 +0300

    add new homework (func and decorators)

--- homework_6.py
+++ homework_6.py
@@ -72,3 +72,194 @@ try:
         print(letter, count)
 except TypeError:
     print ("TypeError")
+
+#homeworf func
+#1
+def min_of_two (a, b):
+   if a < b:
+       return a
+   else:
+       return b
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+number4 = int(input())
+min1 = min_of_two(number1, number2)
+min2 = min_of_two(number2, number3)
+min_of_4 = min_of_two (min1, min2)
+print (min_of_4)
+
+#2
+number = int(input())
+sum = 0
+for divisor in range(1, number):
+   if number % divisor == 0:
+       sum += divisor
+if sum == number:
+   print ("YES")
+else:
+   print ("NO")
+
+#3
+def fib (n):
+   if n == 0:
+       return 0
+   if n == 1:
+       return 1
+   previous_number = 0
+   current_number = 1
+   for i in range (2, n + 1):
+       next_number = previous_number + current_number
+       previous_number = current_number
+       current_number = next_number
+   return next_number
+n = int(input())
+print(fib(n))
+
+#4
+def closest_mod_5 (num):
+   after_division = num % 5
+   if after_division == 0:
+       return num
+   else:
+       return num + (5 - after_division)
+x = int(input())
+print(closest_mod_5(x))
+
+#5
+def check_variable (variable_name):
+   if variable_name[0].isdigit():
+       return False
+   forbidden_symbols = ": ! @ # $ % ^ & * ()"
+   for symbol in variable_name:
+       if not (symbol.isdigit() or symbol == "_"):
+           return False
+       if symbol in forbidden_symbols:
+           return False
+   return True
+while True:
+   user_input = input()
+   if user_input == "Поработали, и хватит":
+       break
+   if check_variable(user_input):
+       print("Можно использовать")
+   else:
+       print ("Нельзя использовать")
+
+#6
+digit_num =[]
+for number in range(11, 100, 2):
+   digit_num.append(number)
+print(digit_num)
+
+#7
+numbers =[]
+for num in range (100, 1000)
+   if number % 3 == 0 and number % 5 == 0:
+       numbers.append(num)
+print(numbers)
+
+#8
+def unique_elem(sorted_list):
+   if len(sorted_list) == 0:
+       return 0
+   unique_count = 1
+   for index in range (1, len(sorted_list)):
+       if sorted_list[index] != sorted_list[index-1]:
+           unique_count += 1
+   return unique_count
+numbers = list(map(int, input().split()))
+print (unique_elem(numbers))
+
+#9
+numbers = list(map(int, input().split()))
+lenght = len(numbers)
+if lenght == 1:
+   print(numbers[0])
+else:
+   result = []
+   for i in range(lenght):
+       left_neighbor = numbers[i - 1]
+       right_neighbor = numbers[(i + 1) % lenght]
+       result.append(left_neighbor + right_neighbor)
+   print(*result)
+
+#decorators
+#1
+def log_result(function):
+   def wrapper(number):
+       result = function(number)
+       print(result)
+       return result
+   return wrapper
+@log_result
+def square(number):
+   return number * number
+square(int(input()))
+
+#2
+def repeat(n):
+   def decorator_repeat(func):
+       def wrapper():
+           result = None
+           for _ in range(n):
+               result = func()
+           return result
+       return wrapper
+   return decorator_repeat
+@repeat(int(input()))
+def hello_world():
+   print('Hello World!')
+   return "nice!"
+final_result = hello_world()
+print(f"Last resul: {final_result}")
+
+#3
+def bench (func):
+   def wrapper():
+       try:
+           return func()
+       except Exception as error:
+           print("Type of error:", type(error).__name__)
+           print("Сообщение:", error)
+   return wrapper()
+@bench
+def divide_by_zero():
+   return 100/0
+divide_by_zero()
+
+#4
+words = ["keyboard", "mouse", "processor"]
+word_lengths = list(map(len, words))
+print(word_lengths)
+
+#5
+words = ['apple', 'Banana', 'cherry', 'DATE']
+lowercase_words = list(filter(str.islower, words))
+print(lowercase_words)
+
+#6
+people =[("Карина", 14),("Дмитрий", 37), ("Екатерина", 22)]
+adults = list(filter(lambda person: person[1] > 18, people))
+print(adults)
+
+#7
+from functools import reduce
+lists = [[1,2],[3,4],[5,6]]
+next_list = reduce(lambda x,y: x+y, lists)
+print(next_list)
+
+#8
+list = ['cat','car','mouse','dog','snake','cow']
+result = {}
+for word in list:
+   first_letter = word[0]
+   if first_letter not in result:
+       result[first_letter] = []
+   result[first_letter].append(word)
+print(result)
+
+#9
+products = [("milk", 2, 5), ("bread", 1, 3), ("cola", 4, 10)]
+total_price = list(map(lambda product: product[1] * product[2], products))
+print(total_price)
